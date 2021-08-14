31510f9e4cd2230846ddc3328514a3c3b0b5d58e
de1ux <nathan@de1ux.com>
2021-08-13 21:25:04 -0600

    Add helpful command output, more test pdfs

--- librarian/api/management/commands/worker.py
+++ librarian/api/management/commands/worker.py
@@ -11,10 +11,16 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("Running worker...")
+        running = False
         while True:
             for i in DocumentJob.objects.filter(completed_at__isnull=True):
+                running = True
                 logger.info(f"Running job '{i.job}'...")
                 i.run()
                 logger.info(f"Running job '{i.job}'...done")
 
-            sleep(1)
+            if running:
+                logger.info("\nNo more jobs, sleeping\n")
+                running = False
+
+            sleep(2)
