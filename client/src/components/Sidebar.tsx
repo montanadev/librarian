f29cc0fb6f9a43bad91c55b8a94b151575539875
de1ux <nathan@de1ux.com>
2022-03-10 15:43:15 -0700

    Even better folder layout

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,13 +1,13 @@
 import './Sidebar.css';
 import {Layout, Menu, Typography} from "antd";
-import {FileAddOutlined, FolderAddOutlined, RadiusUprightOutlined} from "@ant-design/icons";
+import {FileAddOutlined, FolderAddOutlined} from "@ant-design/icons";
 import React, {useState} from "react";
 import {Link, useParams} from "react-router-dom";
 import {Api} from "../utils/Api";
 import {CreateFolderModal} from "./modals/CreateFolderModal";
 import {FolderModel} from "../models/Folder";
 import {ResourceModel} from "../models/Resource";
-import {useQuery, useQueryClient} from 'react-query';
+import {useQuery} from 'react-query';
 
 const {Paragraph} = Typography;
 const {SubMenu} = Menu;
@@ -22,14 +22,13 @@ function Sidebar() {
     const folders = useQuery<ResourceModel<FolderModel>>("folders", api.getFolders);
 
     const openDocuments = [`document-${documentId}`];
-    const openFolders = [`folder-${folderId}`];
 
     return <div key={`sidebar-${documentId}-${folderId}`}>
         <CreateFolderModal visible={createFolderOpen} onClose={() => setCreateFolderOpen(false)}/>
         <Sider width={200} className="site-layout-background">
             <Menu mode="inline"
                   defaultSelectedKeys={openDocuments}
-                  defaultOpenKeys={openFolders}
+                  defaultOpenKeys={["folders"]}
                   style={{height: '100%', borderRight: 0}}>
                 <Menu.Item key="upload" icon={<FileAddOutlined/>}>
                     <Link to="/">Upload</Link>
@@ -38,18 +37,19 @@ function Sidebar() {
                     Add Folder
                 </Menu.Item>
 
-                <Menu.Divider />
+                <Menu.Divider/>
 
-                {folders.data ? folders.data.results.map((f: FolderModel) =>
-                    <SubMenu key={`folder-${f.id}`} title={f.name}
-                             icon={f.name === 'Unsorted' ? <RadiusUprightOutlined/> : null}>
-                        {f.documents ? f.documents.map(d => {
-                            return <Menu.Item key={`document-${d.id}`}>
-                                <Link to={`/folders/${f.id}/documents/${d.id}`}>{d.filename}</Link>
-                            </Menu.Item>
-                        }) : []}
-                    </SubMenu>
-                ) : null}
+                <SubMenu key={"folders"} title={"Folders"}>
+                    {folders.data ? folders.data.results.map((f: FolderModel) =>
+                        <Menu.ItemGroup key={`folder-${f.id}`} title={f.name}>
+                            {f.documents ? f.documents.map(d => {
+                                return <Menu.Item key={`document-${d.id}`}>
+                                    <Link to={`/folders/${f.id}/documents/${d.id}`}>{d.filename}</Link>
+                                </Menu.Item>
+                            }) : []}
+                        </Menu.ItemGroup>
+                    ) : null}
+                </SubMenu>
             </Menu>
         </Sider>
     </div>
