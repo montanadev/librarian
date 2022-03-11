0de9a02d3221c73cc795f3c3de42cdb53112251a
de1ux <nathan@de1ux.com>
2022-03-10 21:16:27 -0700

    Switch to axios, add prettier

--- client/src/utils/Api.ts
+++ client/src/utils/Api.ts
@@ -5,75 +5,40 @@ import axios from "axios";
 
 export class Api {
     getDocuments(): Promise<ResourceModel<DocumentModel>> {
-        return fetch("http://localhost:8000/api/documents/").then(d => d.json());
+        return axios.get("/api/documents/").then(d => d.data);
     }
 
     refreshJob(jobId: number): Promise<JobModel> {
-        return fetch(`/api/documents/${jobId}/details`).then(d => d.json());
+        return axios.get(`/api/documents/${jobId}/details`).then(d => d.data);
     }
 
     uploadDocuments(file: any) {
-        return axios.post(`/api/documents/${file.name}`, {
-            body: file,
-        }).then(d => d.data).catch((error: any) => {
+        return axios.post(`/api/documents/${file.name}`, file).then(d => d.data).catch((error: any) => {
             return Promise.reject(error.response.data.reason)
         });
     }
 
     saveConfig(data: any) {
-        return fetch('/api/config/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data),
-        }).then(res => res.json());
+        return axios.post('/api/config/', data).then(d => d.data);
     }
 
     createFolder(folderName: string) {
-        return fetch('/api/folders/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({name: folderName, documents: []}),
-        }).then(() => {
-
-            console.log("Invalidated!");
-        });
+        return axios.post('/api/folders/', {name: folderName, documents: []});
     }
 
     addDocumentToFolder(documentId: number, folderId: number) {
-        return fetch(`/api/folders/${folderId}/document`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id: documentId}),
-        });
+        return axios.put(`/api/folders/${folderId}/document`, {id: documentId});
     }
 
     getFolders() {
-        return fetch('/api/folders/').then(d => d.json());
+        return axios.get('/api/folders/').then(d => d.data);
     }
 
     renameFolder(folderId: number, newFolderName: string) {
-        return fetch(`/api/folders/${folderId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id: folderId, name: newFolderName, documents: []}),
-        });
+        return axios.put(`/api/folders/${folderId}`, {id: folderId, name: newFolderName, documents: []});
     }
 
     renameDocument(documentId: number, newDocumentName: string) {
-        return fetch(`/api/documents/${documentId}/details`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id: documentId, filename: newDocumentName}),
-        });
+        return axios.put(`/api/documents/${documentId}/details`, {id: documentId, filename: newDocumentName});
     }
 }
