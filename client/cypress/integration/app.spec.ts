7fd4a08c4725820cc7ff19fdae54bcd2fd17d807
de1ux <nathan@de1ux.com>
2022-04-05 09:58:56 -0600

    Add a png integration test

--- client/cypress/integration/app.spec.ts
+++ client/cypress/integration/app.spec.ts
@@ -3,7 +3,7 @@ describe("Librarian", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("should upload and render a document", () => {
+  it("should upload and render a pdf", () => {
     cy.get('[data-cy="dropzone"]').attachFile("sample.pdf", {
       subjectType: "drag-n-drop",
     });
@@ -11,5 +11,13 @@ describe("Librarian", () => {
     cy.contains("click to view", { timeout: 30000 }).click();
     // the rendered document should have an svg with text 'snowflake_2'
     cy.contains("snowflake_2", { timeout: 30000 });
+  });
+
+  it("should upload and render a png", () => {
+    cy.get('[data-cy="dropzone"]').attachFile("sample.png", {
+      subjectType: "drag-n-drop",
+    });
+    cy.contains("click to view", { timeout: 30000 }).click();
+    cy.contains("least", { timeout: 30000 });
   });
 });
