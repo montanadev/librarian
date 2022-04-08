c745bf376104932b9da881f26601c04438d935f5
de1ux <nathan@de1ux.com>
2022-04-08 01:36:14 -0600

    Rewrite viewer as functional component

--- client/src/components/virtual_viewer/Viewer.tsx
+++ client/src/components/virtual_viewer/Viewer.tsx
@@ -1,68 +1,27 @@
-import React, { PureComponent, Fragment, createRef } from "react";
-import PropTypes from "prop-types";
+import React, { Fragment, useRef, useState } from "react";
 import { Document } from "react-pdf/dist/esm/entry.webpack";
 import { VariableSizeList } from "react-window";
 import PageRenderer from "./PageRenderer";
 
-class Viewer extends PureComponent {
-  static propTypes = {
-    scale: PropTypes.number.isRequired,
-    file: PropTypes.string.isRequired,
-    width: PropTypes.number.isRequired,
-  };
+interface Props {
+  scale: number;
+  width: number;
+  file: string;
+}
 
-  static defaultProps = {
-    scale: 1.0,
-  };
-  private _list: React.RefObject<unknown>;
-  private _callResizeHandler: OmitThisParameter<() => void>;
-  private _callOrientationChangeHandler: any;
-  private _mounted: boolean = false;
+export default function Viewer({ scale = 1.0, width, file }: Props) {
+  const [pdf, setPdf] = useState<any>();
+  const [currentPage, setCurrentPage] = useState(0);
+  const [cachedPageDimensions, setCachedPageDimensions] =
+    useState<Map<any, any>>();
+  const [responsiveScale, setResponsiveScale] = useState(1);
+  const [pageNumbers, setPageNumbers] = useState<Map<any, any>>(new Map());
+  const [pages, setPages] = useState<Map<any, any>>(new Map());
+  const [containerHeight, setContainerHeight] = useState(window.innerHeight);
 
-  constructor(props: any) {
-    super(props);
+  const listRef = useRef<any>();
 
-    this.state = {
-      containerHeight: window.innerHeight,
-      pdf: null,
-      currentPage: 1,
-      cachedPageDimensions: null,
-      responsiveScale: 1,
-      pageNumbers: new Map(),
-      pages: new WeakMap(),
-    };
-
-    this._list = createRef();
-
-    this._callResizeHandler = () => this.handleResize();
-    this._callOrientationChangeHandler = () => this.handleResize();
-  }
-
-  componentDidMount() {
-    this._mounted = true;
-    window.addEventListener("resize", this._callResizeHandler);
-    window.addEventListener(
-      "orientationchange",
-      this._callOrientationChangeHandler
-    );
-  }
-
-  componentWillUnmount() {
-    this._mounted = false;
-    window.removeEventListener("resize", this._callResizeHandler);
-    window.removeEventListener(
-      "orientationchange",
-      this._callOrientationChangeHandler
-    );
-  }
-
-  /**
-   * Load all pages so we can cache all page dimensions.
-   *
-   * @param {Object} pdf
-   * @returns {void}
-   */
-  cachePageDimensions(pdf: any) {
+  const cachePageDimensions = (pdf: any) => {
     const promises = Array.from({ length: pdf.numPages }, (v, i) => i + 1).map(
       (pageNumber) => {
         return pdf.getPage(pageNumber);
@@ -73,112 +32,92 @@ class Viewer extends PureComponent {
     // load the first page and use its height for determining all the row
     // heights.
     Promise.all(promises).then((pages) => {
-      if (!this._mounted) {
-        return;
-      }
-
       const pageDimensions = new Map();
 
       for (const page of pages) {
-        const w = page.view[2] * (this.props as any).scale;
-        const h = page.view[3] * (this.props as any).scale;
+        const w = page.view[2] * scale;
+        const h = page.view[3] * scale;
 
         pageDimensions.set(page._pageIndex + 1, [w, h]);
       }
 
-      this.setState({ cachedPageDimensions: pageDimensions });
+      setCachedPageDimensions(pageDimensions);
     });
-  }
+  };
 
-  recomputeRowHeights() {
-    (this._list as any).current.resetAfterIndex(0);
-  }
+  const recomputeRowHeights = () => {
+    listRef.current.resetAfterIndex(0);
+  };
 
-  computeRowHeight(index: any) {
-    const { cachedPageDimensions, responsiveScale } = this.state as any;
-
+  const computeRowHeight = (index: any) => {
     if (cachedPageDimensions && responsiveScale) {
       console.log(responsiveScale);
       return cachedPageDimensions.get(index + 1)[1] / responsiveScale;
     }
 
     return 768; // Initial height
-  }
+  };
 
-  onDocumentLoadSuccess(pdf: any) {
-    this.setState({ pdf });
-    this.cachePageDimensions(pdf);
-  }
+  const onDocumentLoadSuccess = (pdf: any) => {
+    setPdf(pdf);
+    cachePageDimensions(pdf);
+  };
 
-  updateCurrentVisiblePage({ visibleStopIndex }: any) {
-    this.setState({ currentPage: visibleStopIndex + 1 });
-  }
+  const updateCurrentVisiblePage = ({ visibleStopIndex }: any) => {
+    setCurrentPage(visibleStopIndex + 1);
+  };
 
-  computeResponsiveScale(pageNumber: number) {
-    const { cachedPageDimensions, pages, pageNumbers } = this.state as any;
-
+  const computeResponsiveScale = (pageNumber: number) => {
+    if (!pages || !pageNumbers || !cachedPageDimensions) {
+      return;
+    }
     const node = pages.get(pageNumbers.get(pageNumber));
 
     if (!node) return;
 
     return cachedPageDimensions.get(pageNumber)[1] / node.clientHeight;
-  }
+  };
 
-  handleResize() {
-    const { currentPage, responsiveScale } = this.state as any;
-
+  const handleResize = () => {
     // Recompute the responsive scale factor on window resize
-    const newResponsiveScale = this.computeResponsiveScale(currentPage);
+    const newResponsiveScale = computeResponsiveScale(currentPage);
 
     if (newResponsiveScale && responsiveScale !== newResponsiveScale) {
-      this.setState({ responsiveScale: newResponsiveScale }, () =>
-        this.recomputeRowHeights()
-      );
+      setResponsiveScale(responsiveScale);
+      recomputeRowHeights();
     }
 
-    this.setState({ containerHeight: window.innerHeight });
-  }
+    setContainerHeight(window.innerHeight);
+  };
 
-  handleClick(index: any) {
-    (this._list as any).current.scrollToItem(index);
-  }
-
-  render() {
-    const { scale, file, width } = this.props as any;
-    const { cachedPageDimensions, containerHeight, pdf, pages, pageNumbers } =
-      this.state as any;
-
-    return (
-      <Document
-        file={file}
-        onLoadSuccess={this.onDocumentLoadSuccess.bind(this)}
-        onLoadError={(error) => console.error(error)} // eslint-disable-line no-console
-      >
-        {cachedPageDimensions && (
-          <Fragment>
-            <VariableSizeList
-              height={containerHeight}
-              width={width}
-              itemCount={pdf.numPages}
-              itemSize={this.computeRowHeight.bind(this)}
-              itemData={{
-                scale,
-                pages,
-                pageNumbers,
-                numPages: pdf.numPages,
-                triggerResize: this.handleResize.bind(this),
-              }}
-              overscanCount={2}
-              onItemsRendered={this.updateCurrentVisiblePage.bind(this)}
-              ref={this._list as any}
-            >
-              {PageRenderer}
-            </VariableSizeList>
-          </Fragment>
-        )}
-      </Document>
-    );
-  }
+  return (
+    <Document
+      file={file}
+      onLoadSuccess={onDocumentLoadSuccess}
+      onLoadError={(error) => console.error(error)} // eslint-disable-line no-console
+    >
+      {cachedPageDimensions && (
+        <Fragment>
+          <VariableSizeList
+            height={containerHeight}
+            width={width}
+            itemCount={pdf.numPages}
+            itemSize={computeRowHeight}
+            itemData={{
+              scale,
+              pages,
+              pageNumbers,
+              numPages: pdf.numPages,
+              triggerResize: handleResize,
+            }}
+            overscanCount={2}
+            onItemsRendered={updateCurrentVisiblePage}
+            ref={listRef}
+          >
+            {PageRenderer}
+          </VariableSizeList>
+        </Fragment>
+      )}
+    </Document>
+  );
 }
-
-export default Viewer;
